933449ad0bd74d58423c163942b4c28139ea8638
Akronym <prosperfajnzyn@gmail.com>
2026-09-22 12:12:42 +0200

    changement de mail en email afin de matcher la bdd

--- src/repository/UtilisateurRepository.php
+++ src/repository/UtilisateurRepository.php
@@ -13,7 +13,7 @@ class UtilisateurRepository{
         $req->bindValue(':id_utilisateur', $id_utilisateur);
         $req->execute();
         $result = $req->fetch();
-        $utilisateur = new Utilisateur($result["id_utilisateur]"],$result["nom"],$result['prenom'],$result['mail'],$result['mdp'],$result['date_inscription'],$result['statut_validation']);
+        $utilisateur = new Utilisateur($result["id_utilisateur]"],$result["nom"],$result['prenom'],$result['email'],$result['mdp'],$result['date_inscription'],$result['statut_validation']);
         return $utilisateur;
     }
 
@@ -24,31 +24,33 @@ class UtilisateurRepository{
         $result = $req->fetchAll();
         $tabUtilisateur = array();
         foreach ($result as $results) {
-            $tabUtilisateur[] = new Utilisateur ($results["id_utilisateur"],$results["nom"],$results['prenom'],$results['mail'],$results['mdp'],$results['date_inscription'],$results['statut_validation']);}
+            $tabUtilisateur[] = new Utilisateur ($results["id_utilisateur"],$results["nom"],$results['prenom'],$results['email'],$results['mdp'],$results['date_inscription'],$results['statut_validation']);}
         return $tabUtilisateur;
         }
 
 
         public function ajoutUtilisateur(Utilisateur $utilisateur){
-        $sql="INSERT INTO utilisateur VALUES (:id_utilisateur,:nom,:prenom,:mail,:mdp,:statut_validation)";
+        $sql="INSERT INTO utilisateur VALUES (:id_utilisateur,:nom,:prenom,:email,:mdp, :date_inscription, :statut_validation)";
         $req = $this->connexionbdd->prepare($sql);
         $req->bindValue(':id_utilisateur', $utilisateur-> getIdUtilisateur());;
         $req->bindValue(":nom",$utilisateur-> getNom());
         $req->bindValue(":prenom",$utilisateur-> getPrenom());
-        $req->bindValue(":mail",$utilisateur-> getEmail());
+        $req->bindValue(":email",$utilisateur-> getEmail());
         $req->bindValue(":mdp",$utilisateur-> getMdp());
+        $req->bindValue(":date_inscription",$utilisateur-> getDateInscription());
         $req->bindValue(":statut_validation",$utilisateur-> getStatutValidation());
         $req->execute();
         }
 
         public function modifierUtilisateur(Utilisateur $utilisateur){
-        $sql="UPDATE utilisateur SET nom = :nom, prenom =:prenom, mail=:mail, mdp=:mdp, statut_validation=:statut_validation WHERE id_utilisateur = :id_utilisateur";
+        $sql="UPDATE utilisateur SET nom = :nom, prenom =:prenom, email=:email, mdp=:mdp, date_inscription=:date_inscription, statut_validation=:statut_validation WHERE id_utilisateur = :id_utilisateur";
         $req = $this->connexionbdd->prepare($sql);
         $req->bindValue(':id_utilisateur', $utilisateur-> getIdUtilisateur());
         $req->bindValue(':nom',$utilisateur-> getNom());
         $req->bindValue(':prenom',$utilisateur-> getPrenom());
-        $req->bindValue(':mail',$utilisateur-> getEmail());
+        $req->bindValue(':email',$utilisateur-> getEmail());
         $req->bindValue(':mdp',$utilisateur-> getMdp());
+        $req->bindValue(':date_inscription',$utilisateur-> getDateInscription());
         $req->bindValue(':statut_validation',$utilisateur-> getStatutValidation());
         $req->bindValue(':id_utilisateur',$utilisateur-> getIdUtilisateur());
         $req->execute();
