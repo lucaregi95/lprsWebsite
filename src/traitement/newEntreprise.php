7ea686c5e996ceeb6a24d68c9c32dbd0fd4daadd
William <william.lemaire06@gmail.com>
2026-09-22 12:12:38 +0200

    creation du fichier newEntreprise

--- src/traitement/newEntreprise.php
+++ src/traitement/newEntreprise.php
@@ -1,1 +1,12 @@
 <?php
+
+require_once "../bdd/bdd.php";
+require_once "../model/Entreprise.php";
+
+
+if(isset($_POST['adresse']) && isset($_POST['nom_entreprise']) && isset($_POST['site_web'])){
+    $id_entreprise = (isset($_POST['id_entreprise'])) ? $_POST['id_entreprise'] : null;
+
+
+    $entreprise = new Entreprise($id_entreprise,$_POST['adresse'],$_POST['nom_entreprise'],$_POST['site_web']);
+}
