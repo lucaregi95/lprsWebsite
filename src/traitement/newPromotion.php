97e0c99341073bd89ff07c2f1fa95942f0c52792
lucaregi95 <lucaregi95@gmail.com>
2026-09-22 11:08:46 +0200

    mise en place des traitements

--- src/traitement/newPromotion.php
+++ src/traitement/newPromotion.php
@@ -1,1 +1,10 @@
 <?php
+require_once "../bdd/bdd.php";
+require_once "../model/Promotion.php";
+if(isset($_POST['annee'])) {
+    $id_promotion = (isset($_POST['id_promotion'])) ? $_POST['id_promotion'] : null;
+    $annee = (isset($_POST['annee'])) ? $_POST['annee'] : null;
+
+    $promotion = new Promotion($id_promotion, $annee);
+}
+
