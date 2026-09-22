9571c21b2338ca78a60206d3c0747ba136004d2d
Akronym <prosperfajnzyn@gmail.com>
2026-09-22 12:08:05 +0200

    modifcation de la var tabEntreprise et correction de l'orthographe de adresse

--- src/repository/EntrepriseRepository.php
+++ src/repository/EntrepriseRepository.php
@@ -13,7 +13,7 @@ class EntrepriseRepository{
         $req->bindValue(':id_entreprise', $id_entreprise);
         $req->execute();
         $result = $req->fetch();
-        $entreprise = new Entreprise($result["id_entreprise]"],$result["addresse"],$result['nom_entreprise'],$result['site_wab']);
+        $entreprise = new Entreprise($result["id_entreprise]"],$result["adresse"],$result['nom_entreprise'],$result['site_wab']);
         return $entreprise;
     }
 
@@ -24,14 +24,14 @@ class EntrepriseRepository{
         $result = $req->fetchAll();
         $entreprises = array();
         foreach ($result as $results) {
-            $tabEntreprise[] = new Entreprise($results["id_entreprise]"],$results["addresse"],$results['nom_entreprise'],$results['site_web']);
+            $tabEntreprise[] = new Entreprise($results["id_entreprise]"],$results["adresse"],$results['nom_entreprise'],$results['site_web']);
         }
         return $entreprises;
     }
 
 
     public function ajoutEntreprise(Entreprise $entreprise){
-        $sql="INSERT INTO entreprise VALUES (:id_entreprise,:addresse,:nom_entreprise,:site_web)";
+        $sql="INSERT INTO entreprise VALUES (:id_entreprise,:adresse,:nom_entreprise,:site_web)";
         $req = $this->connexionbdd->prepare($sql);
         $req->bindValue(':id_entreprise', $entreprise-> getidEntreprise());;
         $req->bindValue(":addressse",$entreprise-> getAdresse());
@@ -41,9 +41,9 @@ class EntrepriseRepository{
     }
 
     public function modifierEntreprise(Entreprise $entreprise){
-        $sql="UPDATE entreprise SET addresse = :addresse, nom_entreprise =:nom_entreprise, site_web=:site_web WHERE id_entreprise = :id_entreprise";
+        $sql="UPDATE entreprise SET adresse = :adresse, nom_entreprise =:nom_entreprise, site_web=:site_web WHERE id_entreprise = :id_entreprise";
         $req = $this->connexionbdd->prepare($sql);
-        $req->bindValue(':addresse', $entreprise-> getAdresse());
+        $req->bindValue(':adresse', $entreprise-> getAdresse());
         $req->bindValue(':nom_entreprise',$entreprise-> getNomEntreprise());
         $req->bindValue(':site_web',$entreprise-> getSiteWeb());
         $req->bindValue(':id_entreprise',$entreprise-> getidEntreprise());
