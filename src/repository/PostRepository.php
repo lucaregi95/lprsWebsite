8a2c40dd3d8e1b6eab23c253c6c62aae702cb9c1
Akronym <prosperfajnzyn@gmail.com>
2026-09-22 12:11:41 +0200

    changement de date et heure en datetime afin de matcher la bdd

--- src/repository/PostRepository.php
+++ src/repository/PostRepository.php
@@ -13,7 +13,7 @@ class PostRepository{
         $req->bindValue(':id_post', $id_post);
         $req->execute();
         $result = $req->fetch();
-        $post = new Post($result["id_post"],$result["contenu"],$result["date"],$result["heure"]);
+        $post = new Post($result["id_post"],$result["contenu"],$result["datetime"],$result["ref_utilisateur"]);
         return $post;
 
     }
@@ -25,19 +25,18 @@ class PostRepository{
         $result = $req->fetchAll();
         $tabPost = array();
         foreach($result as $post){
-            $post = new Post($result["id_post"],$result["contenu"],$result["date"],$result["heure"]);
-            $tabPost[] = $post;
+            $tabPost = new Post($result["id_post"],$result["contenu"],$result["datetime"],$result["ref_utilisateur"]);
         }
         return $tabPost;
     }
 
     public function ajouterPost(Post $id_post){
-        $sql = "INSERT INTO post(id_post,contenu,date,heure) VALUES(:id_post,:contenu,:date,:heure)";
+        $sql = "INSERT INTO post(id_post,contenu,datetime,ref_utilisateur) VALUES(:id_post,:contenu,:datetime,:ref_utilisateur)";
         $req = $this->connexionbdd->prepare($sql);
         $req->bindValue(':id_post',$id_post->getIdPost());
         $req->bindValue(':contenu',$id_post->getContenu());
-        $req->bindValue(':date',$id_post->getDate());
-        $req->bindValue(':heure',$id_post->getHeure());
+        $req->bindValue(':datetime',$id_post->getDateTime());
+        $req->bindValue(':ref_utilisateur',$id_post->getRefUtilisateur());
         $req->execute();
 
     }
@@ -50,11 +49,11 @@ class PostRepository{
     }
 
     public function modifierPost(Post $post){
-        $sql = "UPDATE post SET contenu=:contenu, date=:date, heure=:heure WHERE id_post=:id_post";
+        $sql = "UPDATE post SET contenu=:contenu, datetime=:datetime, ref_utilisateur=:ref_utilisateur WHERE id_post=:id_post";
         $req = $this->connexionbdd->prepare($sql);
         $req->bindValue(':contenu',$post->getContenu());
-        $req->bindValue(':date',$post->getDate());
-        $req->bindValue(':heure',$post->getHeure());
+        $req->bindValue(':datetime',$post->getDateTime());
+
         $req->bindValue(':id_post',$post->getIdPost());
         $req->execute();
 
